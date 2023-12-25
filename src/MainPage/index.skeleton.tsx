524d60dded40ed5abd2370aa6d47cc085b634473
JitHoon <imessi0336@gmail.com>
2023-12-25 19:13:56 +0900

    feat: main 페이지 전체 fallback 컴포넌트 생성

---
+++ src/MainPage/index.skeleton.tsx
@@ -0,0 +1,19 @@
+const MainPageSkeleton = () => (
+  <div style={{ color: "gray" }}>
+    <div className="header">메인 페이지 로딩</div>
+    <div className="banner__container">
+      <div className="banner">메인 페이지 로딩</div>
+    </div>
+    <div className="main">
+      <div className="custom-menu__container">
+        <div className="menu">메인 페이지 로딩</div>
+      </div>
+      <div className="user__container">
+        <div className="user-info__container">메인 페이지 로딩</div>
+        <div className="alarm__container">메인 페이지 로딩</div>
+      </div>
+    </div>
+  </div>
+);
+
+export default MainPageSkeleton;
