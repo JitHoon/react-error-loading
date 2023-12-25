c33dcdbfb4f90a14919030b27234f7dacb89e437
JitHoon <imessi0336@gmail.com>
2023-12-25 19:10:15 +0900

    feat: QueryClient 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+/* 
+
+v5 부터 suspense: boolean 옵션은 제거되고
+useSuspenseQuery, useSuspenseInfiniteQuery와 useSuspenseQueries를 사용합니다.
+
+*/
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>,
-)
+  </QueryClientProvider>
+);
